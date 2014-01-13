a20015ed4943e592224bf21142b489f5fecb5cbc
Tony Fabeen <tony.fabeen@gmail.com>
2014-01-13 10:37:43 -0200

    Initializes a Jail with the current directory

--- lib/jailman/jail.rb
+++ lib/jailman/jail.rb
@@ -1,5 +1,11 @@
 #encoding: utf-8
 module Jailman
   class Jail
+    attr_accessor :directory
+
+    def initialize
+      @directory = Dir.pwd
+    end
+
   end
 end
--- spec/jailman/jail_spec.rb
+++ spec/jailman/jail_spec.rb
@@ -2,5 +2,14 @@ require 'spec_helper'
 require 'jailman/jail'
 
 describe Jailman::Jail do
-  pending
+
+  context 'when initialized' do
+    let(:jail) { described_class.new }
+
+    it 'populates directory' do
+      expect(jail.directory).to_not be_nil
+      expect(jail.directory).to match(Dir.pwd)
+    end
+
+  end
 end
