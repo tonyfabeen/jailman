21d5b3a60c6f1527444a37b4467883f60a12b209
Tony Fabeen <tony.fabeen@gmail.com>
2014-01-13 10:54:26 -0200

    Jail Object constructor

--- lib/jailman/jail.rb
+++ lib/jailman/jail.rb
@@ -1,10 +1,11 @@
 #encoding: utf-8
 module Jailman
   class Jail
-    attr_accessor :directory
+    attr_accessor :directory, :name
 
-    def initialize
+    def initialize(name=nil)
       @directory = Dir.pwd
+      @name      = name || @directory.split('/').last
     end
 
   end
--- spec/jailman/jail_spec.rb
+++ spec/jailman/jail_spec.rb
@@ -4,12 +4,36 @@ require 'jailman/jail'
 describe Jailman::Jail do
 
   context 'when initialized' do
-    let(:jail) { described_class.new }
 
-    it 'populates directory' do
-      expect(jail.directory).to_not be_nil
-      expect(jail.directory).to match(Dir.pwd)
+    context 'without a name' do
+      let(:jail) { described_class.new }
+
+      it 'populates directory' do
+        expect(jail.directory).to_not be_nil
+        expect(jail.directory).to match(Dir.pwd)
+      end
+
+      it 'and populates name with the last element of directory' do
+        expect(jail.name).to_not be_nil
+        expect(jail.name).to match (jail.directory.split('/').last)
+      end
+
     end
+
+    context 'with a name' do
+      let(:jail) { described_class.new('my_jail') }
+
+      it 'populates directory' do
+        expect(jail.directory).to_not be_nil
+        expect(jail.directory).to match(Dir.pwd)
+      end
+
+      it 'and populates name with the constructor argument' do
+        expect(jail.name).to match('my_jail')
+      end
+
+    end
+
 
   end
 end
