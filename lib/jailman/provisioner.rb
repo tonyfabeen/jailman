14883a7090ef588c53cabac0fb462ed0f1fde282
Tony Fabeen <tony.fabeen@gmail.com>
2014-01-20 15:37:47 -0200

    Removed Unused attributes

--- lib/jailman/provisioner.rb
+++ lib/jailman/provisioner.rb
@@ -4,14 +4,10 @@ module Jailman
   class Provisioner
 
     attr_reader :jail
-    attr_reader :rootfs_script
-    attr_reader :jail_script
 
     def initialize(jail=nil)
       raise ArgumentError.new('A jail must be passed') unless jail
       @jail = jail
-      @rootfs_script = Jailman::Constants::ROOTFS_SCRIPT
-      @jail_script   = Jailman::Constants::JAIL_SCRIPT
     end
 
     def run!
@@ -19,15 +15,19 @@ module Jailman
       create_rootfs
     end
 
+    def status
+      #TODO: NOw
+    end
+
     private
 
     def create_jail
-      command = "#{jail_script} #{jail.name} --create"
+      command = "#{Jailman::Constants::JAIL_SCRIPT} #{jail.name} --create"
       Jailman::CommandRunner.run(command)
     end
 
     def create_rootfs
-      command = "#{rootfs_script} #{jail.name} #{jail.directory}"
+      command = "#{Jailman::Constants::ROOTFS_SCRIPT} #{jail.name} #{jail.directory}"
       Jailman::CommandRunner.run(command)
     end
 
